04fec204c73d15ab2c2f25cefb1145daed4aa6fc
koi0823 <86875051+koi0823@users.noreply.github.com>
2023-11-03 16:21:26 +0800

    change

--- assignment/LoadData.h
+++ assignment/LoadData.h
@@ -15,20 +15,35 @@ public:
         cout << "=======================================================" << endl;
         cout << "                   Data Loaded           " << endl;
         cout << "=======================================================" << endl;
-        weeklyDataTree.display();
+        weeklyDataTree.displayAllData();
         cout << "=======================================================" << endl;
-        
-        // Load Annual Data
-        AnnualDataList* annualDataList = AnnualDataList::getInstance();
-        string file_path2 = "/Users/klin/Documents/GitHub/dstr-ass/assignment/Annual number of dengue cases by state.csv";
-        annualDataList->loadFromCSV(file_path2);
 
-        // Display the loaded data
-        cout << "=======================================================" << endl;
-        cout << "                   Data Loaded           " << endl;
-        cout << "=======================================================" << endl;
-        annualDataList->display();
-        cout << "=======================================================" << endl;
+
+
+        // int weekToDisplay = 2; // Change this to the week you want to display
+
+        // // Iterate over the years and display data for the specified week
+        // vector<int> years;
+        // weeklyData.collectUniqueYears(weeklyData.getRoot(), years); // Collect unique years in the BST
+
+        // for (int year : years) {
+        //     cout << "Year: " << year << endl;
+        //     cout << "Week: " << weekToDisplay << endl;
+        //     weeklyData.displayWeekData(year, weekToDisplay);
+        //     cout << endl;
+        // }
+        
+        // // Load Annual Data
+        // AnnualDataList* annualDataList = AnnualDataList::getInstance();
+        // string file_path2 = "/Users/klin/Documents/GitHub/dstr-ass/assignment/Annual number of dengue cases by state.csv";
+        // annualDataList->loadFromCSV(file_path2);
+
+        // // Display the loaded data
+        // cout << "=======================================================" << endl;
+        // cout << "                   Data Loaded           " << endl;
+        // cout << "=======================================================" << endl;
+        // annualDataList->display();
+        // cout << "=======================================================" << endl;
 
     }
 
--- assignment/WeeklyData.h
+++ assignment/WeeklyData.h
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm> // Added for find
 
 using namespace std;
 
@@ -37,15 +38,28 @@ private:
         }
     }
 
-
-    void display(WeeklyDataNode* node) const {
+    void collectUniqueYears(WeeklyDataNode* node, vector<int>& years) const {
         if (!node) return;
-        display(node->left);
-        cout << "State: " << node->state
-             << ", Year: " << node->year
-             << ", Week: " << node->week
-             << ", Number Dengue Cases: " << node->numOfDengueCases << endl;
-        display(node->right);
+        collectUniqueYears(node->left, years);
+        if (find(years.begin(), years.end(), node->year) == years.end()) {
+            years.push_back(node->year);
+        }
+        collectUniqueYears(node->right, years);
+    }
+
+    void displayWeekData(WeeklyDataNode* node, int year, int week) const {
+        if (!node) return;
+
+        // Display data for the current node if it belongs to the current year and week
+        if (node->year == year && node->week == week) {
+            cout << "Week: " << node->week;
+            cout << ", State: " << node->state;
+            cout << ", Number Dengue Cases: " << node->numOfDengueCases << endl;
+        }
+
+        // Recursively display data for left and right subtrees
+        displayWeekData(node->left, year, week);
+        displayWeekData(node->right, year, week);
     }
 
     void clear(WeeklyDataNode*& node) {
@@ -63,14 +77,6 @@ public:
         insert(root, state, year, week, numOfDengueCases);
     }
 
-    void display() const {
-        if (!root) {
-            cout << "The tree is empty." << endl;
-        } else {
-            display(root);
-        }
-    }
-
     void loadFromCSV(const string& file_path) {
         ifstream file(file_path);
         if (!file.is_open()) {
@@ -81,6 +87,8 @@ public:
         string line;
         vector<string> states;
         int row_index = 0;
+        int year_col = -1;
+        int week_col = -1;
 
         while (getline(file, line)) {
             istringstream sline(line);
@@ -89,36 +97,58 @@ public:
             string state;
 
             while (getline(sline, cell, ',')) {
-                if (row_index == 0 && col_index >= 2) {
-                    states.push_back(cell);
-                } else if (col_index == 0) {
-                    try {
-                        year = stoi(cell);
-                    } catch (const std::invalid_argument& e) {
-                        cerr << "Invalid year: " << cell << endl;
-                        year = 0; // Handle the error by setting a default value.
-                    }
-                } else if (col_index == 1) {
-                    try {
-                        week = stoi(cell);
-                    } catch (const std::invalid_argument& e) {
-                        cerr << "Invalid week: " << cell << endl;
-                        week = 0; // Handle the error by setting a default value.
+                if (row_index == 0) {
+                    if (cell == "TAHUN") {
+                        year_col = col_index;
+                    } else if (cell == "MINGGU EPID") {
+                        week_col = col_index;
+                    } else if (col_index >= 2) {
+                        states.push_back(cell);
                     }
                 } else {
-                    try {
-                        numOfDengueCases = stoi(cell);
-                    } catch (const std::invalid_argument& e) {
-                        cerr << "Invalid number of dengue cases: " << cell << endl;
-                        numOfDengueCases = 0; // Handle the error by setting a default value.
+                    if (col_index == year_col) {
+                        try {
+                            year = stoi(cell);
+                        } catch (const std::invalid_argument& e) {
+                            cerr << "Invalid year: " << cell << endl;
+                            year = 0; // Handle the error by setting a default value.
+                        }
+                    } else if (col_index == week_col) {
+                        try {
+                            week = stoi(cell);
+                        } catch (const std::invalid_argument& e) {
+                            cerr << "Invalid week: " << cell << endl;
+                            week = 0; // Handle the error by setting a default value.
+                        }
+                    } else {
+                        try {
+                            numOfDengueCases = stoi(cell);
+                        } catch (const std::invalid_argument& e) {
+                            cerr << "Invalid number of dengue cases: " << cell << endl;
+                            numOfDengueCases = 0; // Handle the error by setting a default value.
+                        }
+                        insert(states[col_index - 2], year, week, numOfDengueCases);
                     }
-                    insert(states[col_index - 2], year, week, numOfDengueCases);
                 }
                 col_index++;
             }
             row_index++;
         }
         file.close();
+    }
+
+    void displayAllData() const {
+        vector<int> years; // Store unique years
+        collectUniqueYears(root, years); // Collect unique years in the BST
+
+        for (int year : years) {
+            cout << "Year: " << year << endl;
+            for (int week = 1; week <= 52; week++) { // Assuming there are 52 weeks in a year
+                cout << "Week: " << week << endl;
+                displayWeekData(root, year, week); // Display data for the current year and week
+                cout << endl;
+            }
+        }
     }
 
     ~WeeklyDataBST() {
